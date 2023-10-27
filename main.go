8a5eb31fe56e64ebfeb2456accc83d4f1f9a31f7
interysy <2554268w@student.gla.ac.uk>
2023-10-27 22:05:03 +0100

    change variable names to clearly show that they're constants

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 var (
-	orange1 = rl.NewColor(255, 109, 0, 1)
-	orange2 = rl.NewColor(255, 121, 0, 1)
-	orange3 = rl.NewColor(255, 133, 0, 1)
-	orange4 = rl.NewColor(255, 145, 0, 1)
-	orange5 = rl.NewColor(255, 158, 0, 1)
-	purple1 = rl.NewColor(36, 0, 70, 1)
-	purple2 = rl.NewColor(60, 9, 108, 1)
-	purple3 = rl.NewColor(90, 24, 154, 1)
-	purple4 = rl.NewColor(123, 44, 191, 1)
-	purple5 = rl.NewColor(157, 78, 221, 1)
+	ORANGE1 = rl.NewColor(255, 109, 0, 1)
+	ORANGE2 = rl.NewColor(255, 121, 0, 1)
+	ORANGE3 = rl.NewColor(255, 133, 0, 1)
+	ORANGE4 = rl.NewColor(255, 145, 0, 1)
+	ORANGE5 = rl.NewColor(255, 158, 0, 1)
+	PURPLE1 = rl.NewColor(36, 0, 70, 1)
+	PURPLE2 = rl.NewColor(60, 9, 108, 1)
+	PURPLE3 = rl.NewColor(90, 24, 154, 1)
+	PURPLE4 = rl.NewColor(123, 44, 191, 1)
+	PURPLE5 = rl.NewColor(157, 78, 221, 1)
 )
 
 func main() {
@@ -29,7 +29,7 @@ func loginScreen() {
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		// rl.DrawTexture(texture, rl.LoadImageFromScreen().Width/2, rl.LoadImageFromScreen().Height/2, rl.White)
-		rl.ClearBackground(purple1)
+		rl.ClearBackground(PURPLE1)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 
 		rl.EndDrawing()
