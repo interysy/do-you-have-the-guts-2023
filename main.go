0b40afa64dc9d84169247b8cd193c9ac963d5c7b
interysy <2554268w@student.gla.ac.uk>
2023-10-27 21:34:39 +0100

    Test login window with colours

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
+	loginScreen()
+}
+
+func loginScreen() {
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
@@ -10,7 +14,10 @@ func main() {
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 
-		rl.ClearBackground(rl.RayWhite)
+		var colour rl.Color = rl.NewColor(uint8(60), uint8(9), uint8(108), uint8(1))
+
+		rl.ClearBackground(colour)
+		rl.DrawRectangleRounded(rl.Rectangle{400, 225, 20, 20}, 0.1, 10, rl.Black)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 
 		rl.EndDrawing()
