6fa07327ae125a883024032b8c3ded3fe606dc44
interysy <2554268w@student.gla.ac.uk>
2023-10-27 21:51:26 +0100

    Defining global colour palette for the game

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+var (
+	orange1 = rl.NewColor(255, 109, 0, 1)
+	orange2 = rl.NewColor(255, 121, 0, 1)
+	orange3 = rl.NewColor(255, 133, 0, 1)
+	orange4 = rl.NewColor(255, 145, 0, 1)
+	orange5 = rl.NewColor(255, 158, 0, 1)
+	purple1 = rl.NewColor(36, 0, 70, 1)
+	purple2 = rl.NewColor(60, 9, 108, 1)
+	purple3 = rl.NewColor(90, 24, 154, 1)
+	purple4 = rl.NewColor(123, 44, 191, 1)
+	purple5 = rl.NewColor(157, 78, 221, 1)
+)
+
 func main() {
 	loginScreen()
 }
@@ -11,13 +24,13 @@ func loginScreen() {
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 
+	//var texture rl.Texture2D = rl.LoadTexture("assets/texture.png")
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
+		// rl.DrawTexture(texture, rl.LoadImageFromScreen().Width/2, rl.LoadImageFromScreen().Height/2, rl.White)
 
-		var colour rl.Color = rl.NewColor(uint8(60), uint8(9), uint8(108), uint8(1))
-
-		rl.ClearBackground(colour)
-		rl.DrawRectangleRounded(rl.Rectangle{400, 225, 20, 20}, 0.1, 10, rl.Black)
+		rl.ClearBackground(purple1)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 
 		rl.EndDrawing()
