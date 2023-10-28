0d545ec7a76fdb464871720401d7e063a3484d00
interysy <2554268w@student.gla.ac.uk>
2023-10-28 09:40:02 +0100

    fix(particles): fix particle physics for pumpkin

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	rl "github.com/gen2brain/raylib-go/raylib"
 	"math/rand"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 var (
@@ -16,6 +17,8 @@ var (
 	PURPLE3 = rl.NewColor(90, 24, 154, 1)
 	PURPLE4 = rl.NewColor(123, 44, 191, 1)
 	PURPLE5 = rl.NewColor(157, 78, 221, 1)
+
+	CENTRAL = rl.GetScreenWidth() / 2
 )
 
 func main() {
@@ -29,10 +32,16 @@ func loginScreen() {
 
 	//var texture rl.Texture2D = rl.LoadTexture("assets/texture.png")
 
+	var particles = generateParticles(10, centraliseInX(25), 100)
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
+
 		// rl.DrawTexture(texture, rl.LoadImageFromScreen().Width/2, rl.LoadImageFromScreen().Height/2, rl.White)
 		rl.ClearBackground(PURPLE1)
+
+		//rl.DrawRectangleRounded(rl.NewRectangle(centraliseInX(800), 50, 400, 350), 0.5, 0, ORANGE1)
+		updateParticles(particles, int32(CENTRAL))
+		renderParticles(particles)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 
 		rl.EndDrawing()
@@ -41,47 +50,47 @@ func loginScreen() {
 
 func centraliseInX(size int) int32 {
 	var centralXCoordinate = rl.GetScreenWidth()/2 - size/2
-
 	return int32(centralXCoordinate)
 }
 
 type Particle struct {
-	x int32 
+	x int32
 	y int32
 }
 
-
-func generateParticles(num_particles int, x int32, y int32) []rl.Rectangle{
+func generateParticles(num_particles int, x int32, y int32) []rl.Rectangle {
 	var deviation int32 = 10
 	particles := make([]rl.Rectangle, num_particles)
 
 	for i := 0; i < num_particles; i++ {
-		particle = rl.Rectangle{}
-		particle.X = float32(x + rand.Int31n(deviation))
-		particle.Y = float32(y + rand.Int31n(deviation))
-		particle.Width = 25 
-		particle.Height = 25
+		particles[i].X = float32(x + rand.Int31n(deviation))
+		particles[i].Y = float32(y + rand.Int31n(deviation))
+		particles[i].Width = 25
+		particles[i].Height = 25
 	}
 
 	return particles
 }
 
-func updateParticles(particles []rl.Rectangle) []rl.Rectangle{
-	var gravity int32 = 10
+func updateParticles(particles []rl.Rectangle, centralXCoordinate int32) {
+	var deviation int32 = 10
+	var gravity int32 = 1 // Adjust the gravity value for a noticeable effect
 
-	for _, particle := range particles {
-		particle.X += rand.Int31n(deviation)
-		particle.Y += gravity // might need to be -
+	for i := range particles {
+		var min float32 = -1.0
+		var max float32 = 1.0
+		r := min + rand.Float32()*(max-min)
+		particles[i].X += float32(centralXCoordinate) + (r * float32(rand.Int31n(deviation)))
+
+		particles[i].Y -= float32(gravity)
 	}
-  
-	return particles
 }
 
 func renderParticles(particles []rl.Rectangle) {
-	var colour = rl.Gray
-
-	//TODO Add random to colour
+	// Loop through particles and draw each one
 	for _, particle := range particles {
-		rl.DrawRectanglerec(particle, colour)
+		// Generate a random color for each particle
+		color := rl.LightGray
+		rl.DrawRectangleRec(particle, color)
 	}
 }
