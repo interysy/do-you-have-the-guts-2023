bd673ca782d782d2ff8814d09308205796dbc65e
AngeloSingh <angelosingh32@gmail.com>
2023-10-28 12:38:54 +0100

    fix(main loop): no longer lose all variables in the scope upon updating

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -21,16 +22,58 @@ var (
 	CENTRAL = rl.GetScreenWidth() / 2
 )
 
+var state string = "login"
+
 func main() {
-	var state string = "login"
+
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
+	var pumpkins = loadPumpkin()
+
+	var i = 1
+	var particles []rl.Rectangle
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		if state == "login" {
-			loginScreen()
+			//loginScreen()
+			var pumpkin rl.Texture2D = pumpkins[i-1] // = rl.LoadTexture("assets/pumpkins/pumpkin_stage_1.png")
+
+			rl.ClearBackground(PURPLE1)
+
+			xCentralRectangleCoordinate := float32(centraliseInX(90))
+			//var iconRectangle = rl.NewRectangle(xCentralRectangleCoordinate, float32(rl.GetScreenHeight())/4+7, 90, 90)
+
+			// DrawBorderedRectangle(iconRectangle, 5, PURPLE1, rl.White)
+			rl.DrawCircle(int32(xCentralRectangleCoordinate+45), int32(rl.GetScreenHeight()/4+50), 50, rl.DarkPurple)
+
+			//rl.DrawTexturePro(texture, rl.NewRectangle(0, 0, float32(texture.Width), float32(texture.Height)), rl.NewRectangle(posX, posY, width*scaleX, height*scaleY), rl.NewVector2(0, 0), 0, tint)
+			rl.DrawTexture(pumpkin, centraliseInX(int(pumpkin.Width)), int32(rl.GetScreenHeight()/4)+5, rl.White)
+
+			loginUserName := "common jp morgan enjoyer"
+			rl.DrawText(loginUserName, centraliseInX(len(loginUserName)*7), 250, 15, rl.Orange)
+			rl.DrawRectangleRounded(rl.NewRectangle(float32(centraliseInX(200)), 275, 200, 20), 0.1, 0, rl.Orange)
+			rl.DrawText("password", 300, 275, 16, rl.White)
+
+			if rl.CheckCollisionPointCircle(rl.GetMousePosition(), rl.NewVector2(xCentralRectangleCoordinate+45, float32(rl.GetScreenHeight()/4+50)), 50) {
+				if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+					i++
+					if i < 10 {
+						fmt.Println(i)
+						particles = generateParticles(10, centraliseInX(25), 100)
+					}
+					if i == 9 {
+						state = "desktop"
+					}
+					
+				}
+				updateParticles(particles, int32(CENTRAL))
+				renderParticles(particles)
+			}
+		}
+		if state == "desktop" {
+			fmt.Print("desktop")
 		}
 		rl.EndDrawing()
 	}
@@ -55,9 +98,10 @@ func DrawBorderedRectangle(rect rl.Rectangle, borderWidth float32, fillColor rl.
 	rl.DrawRectangle(int32(rect.X+rect.Width-borderWidth), int32(rect.Y), int32(borderWidth), int32(rect.Height), borderColor)
 }
 
-func getPumpkin(pumpkinOrder int) rl.Texture2D {
-
-	var pumpkin = rl.LoadTexture("assets/pumpkins/pumpkin_stage_" + strconv.Itoa(pumpkinOrder) + ".png")
-
+func loadPumpkin() []rl.Texture2D {
+	var pumpkin []rl.Texture2D
+	for i := 1; i < 10; i++ {
+		pumpkin = append(pumpkin, rl.LoadTexture("assets/pumpkins/pumpkin_stage_"+strconv.Itoa(i)+".png"))
+	}
 	return pumpkin
 }
